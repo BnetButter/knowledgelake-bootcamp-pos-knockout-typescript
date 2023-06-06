cf893b24c68a97963d2f612270b3424775376a83
Kevin Lai <kevinlai31@gmail.com>
2023-06-05 19:28:39 -0500

    pull data from menu api

--- src/index.ts
+++ src/index.ts
@@ -66,226 +66,7 @@ interface IMenuItemViewModel extends IMenuItem {
     addItemToCart(): void;
 }
 
-const DATA: IMenu = {
-    "items": [
-        {
-            "id": 0,
-            "name": "Hamburger",
-            "type_id": 0,
-            "price": 425,
-            "options":[22, 23, 24, 25]
-        },
-        {
-            "id": 1,
-            "name": "Cheeseburger",
-            "type_id": 0,
-            "price":470,
-            "options":[22, 23, 24, 25]
-        },
-        {
-            "id":2,
-            "name": "Double Cheeseburger",
-            "type_id": 0,
-            "price": 655,
-            "options":[22, 23, 24, 25]
-        },
-        {
-            "id":3,
-            "name": "Double Hamburger",
-            "type_id": 0,
-            "price": 305,
-            "options":[22, 23, 24, 25]
-        
-        },
-        {
-            "id":4,
-            "name": "Roast Beef",
-            "type_id": 1,
-            "price": 880
-        },
-        {
-            "id":5,
-            "name": "Spaghetti",
-            "type_id": 1,
-            "price": 880
-        },
-        {
-            "id": 6,
-            "name": "Pot Roast",
-            "type_id": 1,
-            "price": 750
-        },
-        {
-            "id": 7,
-            "name": "BBQ Pork",
-            "type_id": 1,
-            "price": 775
-        },
-        {
-            "id": 8,
-            "name": "Blueberry Pie",
-            "type_id": 3,
-            "price": 320
-        },
-        {
-            "id": 9,
-            "name": "Apple Cobbler Pie",
-            "type_id":3,
-            "price": 220
-        },
-        {
-            "id":10,
-            "name": "Salad",
-            "type_id":2,
-            "price": 305
-        },
-        {
-            "id":11,
-            "name": "French Fries",
-            "type_id": 2,
-            "price": 220
-        },
-        {
-            "id":12,
-            "name": "Garlic Bread",
-            "type_id": 2,
-            "price": 175
-        },
-        {
-            "id":13,
-            "name": "Peas",
-            "type_id":2,
-            "price": 175
-        },
-        {
-            "id":14,
-            "name": "Bottled Water",
-            "type_id": 4,
-            "price": 220
-        },
-        {
-            "id":15,
-            "name": "Gatorade",
-            "type_id":4,
-            "price":220
-        },
-        {
-            "id":16,
-            "name":"Tea",
-            "type_id":5,
-            "price":220,
-            "options": [26, 27, 28]
-        },
-        {
-            "id":17,
-            "name": "Sweet Tea",
-            "type_id": 5,
-            "price": 220,
-            "options": [26, 27, 28]
-        },
-        {
-            "id": 18,
-            "name": "Coffee",
-            "type_id": 5,
-            "price": 100,
-            "options": [26, 27, 28]
-        },
-        {
-            "id": 19,
-            "name": "Chips",
-            "type_id": 6,
-            "price":75
-        },
-        {
-            "id":20,
-            "name":"Candy Bar",
-            "type_id":6,
-            "price":150
-        },
-        {
-           "id":21,
-           "name":"Peanuts",
-           "type_id":6,
-           "price":100
-        },
-        {
-            "id":22,
-            "name": "Lettuce",
-            "type_id":7,
-            "price": 0
-        },
-        {
-            "id":23,
-            "name": "Pickles",
-            "type_id":7,
-            "price":0
-        },
-        {
-            "id":24,
-            "name": "Onions",
-            "type_id":7,
-            "price":0
-        },
-        {
-            "id":25,
-            "name": "Tomatoes",
-            "type_id":7,
-            "price":0
-        },
-        {
-            "id":26,
-            "name": "Small Cup",
-            "type_id": 7,
-            "price": 0
-        },
-        {
-            "id":27,
-            "name": "Medium Cup",
-            "type_id": 7,
-            "price":75
-        },
-        {
-            "id":28,
-            "name": "Large Cup",
-            "type_id": 7,
-            "price": 100
-        }
-    ],
-    "types": [
-        {
-            "id": 0,
-            "name": "Sandwich"
-        },
-        {
-            "id": 1,
-            "name": "Dinner"
-        },
-        {
-            "id": 2,
-            "name": "Side"
-        },
-        {
-            "id": 3,
-            "name": "Dessert"
-        },
-        {
-            "id": 4,
-            "name": "Drinks"
-        },
-        {
-            "id": 5,
-            "name": "Fountain Drinks"
-        },
-        {
-            "id": 6,
-            "name": "Snacks"
-        },
-        {
-            "id": 7,
-            "name": "Condiments"
-        }
-    ]
-}
+
 
 
 function ModalOptionViewModel(this: IModalOptionViewModel, id: number, name:string) {
@@ -304,7 +85,7 @@ function ModalViewModel(this:IModalViewModel) {
 
     self.setModal = (val) => { self.showModal(val); return self; }
     self.setOptions = (options) => {
-        self.modalOptions(options.map((id) => new ModalOptionViewModel(DATA.items[id].id, DATA.items[id].name)));
+        self.modalOptions(options.map((id) => new ModalOptionViewModel(getData().items[id].id, getData().items[id].name)));
         return self;
     }
     self.setOnDone = (onDone) => { self.onComplete(onDone); return self; }
@@ -319,7 +100,7 @@ function RootViewModel(this:IRootViewModel)
     
     // Not using a map statement because we need the reference
     let types: IMenuTypeViewModel[] = [];
-    for (let type of DATA.types) {
+    for (let type of getData().types) {
         types.push(new MenuTypeViewModel(type, types, self.shoppingCart, self.modalModel));
     }
     self.types = ko.observableArray(types);
@@ -344,11 +125,11 @@ function ShoppingCartItemModel(this: IShoppingCartItemModel, item: IMenuItem, on
 
     self.price = ko.computed(() => {
         return (!options.length) ? item.price : item.price + options
-            .map((id:number) => DATA.items[id].price)
+            .map((id:number) => getData().items[id].price)
             .reduce((a:number, b:number) => a + b)
     })
     self.displayName = ko.computed(() => {
-        return self.name + options.map((id:number) => "<br/>+ " + DATA.items[id].name).join("")
+        return self.name + options.map((id:number) => "<br/>+ " + getData().items[id].name).join("")
     })
 
     self.displayPrice = ko.computed(() => {
@@ -366,7 +147,7 @@ function MenuTypeViewModel(
 {
     let self = this;
     
-    let items = DATA.items
+    let items = getData().items
         .filter((item) => item.type_id == type.id)
         .map((item) => new MenuItemViewModel(item, shoppingCart, modalModel))
     self.name = type.name
@@ -413,4 +194,31 @@ function MenuItemViewModel(
     }
 }
 
-ko.applyBindings(new RootViewModel());
+let _data: IMenu | null = null;
+
+function getData(): IMenu {
+    if (! _data) {
+        throw "Error"
+    }
+    return _data;
+}
+
+const MENU_API_HOST = "http://localhost:5080"
+
+async function _getData(): Promise<IMenu> {
+    let menuItem = MENU_API_HOST + "/menuitems"
+    let menuType = MENU_API_HOST + "/menutypes"
+
+    let resp = await fetch(menuItem)
+    let resp2 = await fetch(menuType)
+
+    return {
+        items: await resp.json(),
+        types: await resp2.json(),
+    }
+}
+
+_getData()
+    .then((data:IMenu) => _data = data)
+    .then((_) => ko.applyBindings(new RootViewModel()));
+
